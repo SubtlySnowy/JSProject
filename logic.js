174761fec57d62916e376f3ef2556eeb09261b95
abdullah <abdullah_sanio@icloud.com>
2021-05-27 06:02:11 +0300

    js test

--- logic.js
+++ logic.js
@@ -1,25 +1,33 @@
 
-const lmao = document.getElementById("button")
 
 removetodo = () =>{
-    var buttonClicked = event.target
-    buttonClicked.parentElement.remove()
+    const Deletebutton  = document.getElementById('btn')
+    alert("H")
+
+
+
+    
 
 }
+const Deletebutton  = document.getElementsByClassName('btn')
 
-e  = () =>{
+Deletebutton.addEventListener('click', removetodo)
+
+
+// const Deletebutton  = document.getElementsByClassName('btn btn-danger')
+// Deletebutton.addEventListener('click', removetodo)
+
+  createItem= () =>{
     const entry = document.getElementById("todoinput")
-    const todoitem = `            <div class="todoitem"> <p> ${entry.value}</p><button class="btn btn-danger"> delete</button> </div> 
-    ` 
+    const todoitem = `<div class="todoitem"> <p> ${entry.value}</p><button class="btn btn-danger"> delete</button> </div>` 
     var divlist = document.getElementById("todolist")
-    // const button  = document.getElementsByClassName('btn btn-danger')
-    button.addEventListener('click', removetodo())
+    
 
     divlist.innerHTML += todoitem
 }
 
-const button1  = document.getElementById('btn')
+const button1  = document.getElementById('button')
 
-button1.addEventListener("click", e)
+button1.addEventListener("click", createItem)
 
 // lmao.addEventListener("click", e )
